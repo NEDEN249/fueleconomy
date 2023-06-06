cf2a72a27f92d7d53418f63041b7bd278b592f57
nathan eden <nathaneden483@yahoo.com.au>
2023-06-06 16:25:01 +0800

    enters new row data into .xlsx file

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import xlrd
 import xlwt
-import openpyxl as xl
+import openpyxl
+from openpyxl import Workbook, load_workbook
+from datetime import date
+
+today = date.today()
+today1 = today.strftime("%d/%m/%Y")
 
 def calculate_fuel_economy(kilometresDriven, litresUsed): # Function to calculate fuel economy by litres per 100km
     fuelEconomy =  (litresUsed / kilometresDriven) * 100
     return fuelEconomy
+
+def insert_new_data(d,s):
+    book = load_workbook('testDocument.xlsx') # Load workbook
+    sheet = book.active # Load active sheet
+    new_data = (int(d), int(s), today1)
+    sheet.append(new_data)
+    book.save('testDocument.xlsx')
+    print("Data successfully added to spreadsheet")
 
 print("Welcome to the Fuel Economy Calculator") # Welcome message
 print("=======================================")
@@ -15,11 +28,13 @@ type = input("Please enter 1 or 2: ") # User input to choose between 1 or 2
 if type == "1":
     kilometresDriven = input("Please enter the number of kilometres driven: ") # User input for kilometres driven
     litresUsed = input("Please enter the number of litres used: ") # User input for litres used
-    fuelEconomy = calculate_fuel_economy(int(kilometresDriven), int(litresUsed)) # Calling the function to calculate fuel economy
+    fuelEconomy = calculate_fuel_economy(float(kilometresDriven), float(litresUsed)) # Calling the function to calculate fuel economy
     print("Your fuel economy is: " + str(fuelEconomy) + " litres per 100km") # Print fuel economy
     print("would you like to add this data to a spreadsheet?")
     answer = input("Please enter yes or no: ") # User input to choose between yes or no
     if answer == "yes":
-        print("Data added to spreadsheet")
-elif type == "2":
-    print("Summary Data") # Print summary data
+        insert_new_data(kilometresDriven,litresUsed)
+elif type == "2": #summary data 
+    book = load_workbook('testDocument.xlsx') # Load workbook
+    sheet = book.active # Load active sheet
+    
