f0e2cb4b1d8de4804a832644bfef151802d76525
nathan eden <nathaneden483@yahoo.com.au>
2023-06-06 21:17:29 +0800

    loops through values, adds to a dictionary

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def calculate_fuel_economy(kilometresDriven, litresUsed): # Function to calculat
     fuelEconomy =  (litresUsed / kilometresDriven) * 100
     return fuelEconomy
 
-def insert_new_data(d,s):
+def insert_new_data(d,f):
     book = load_workbook('testDocument.xlsx') # Load workbook
     sheet = book.active # Load active sheet
-    new_data = (int(d), int(s), today1)
+    new_data = (today1, int(d), int(f))
     sheet.append(new_data)
     book.save('testDocument.xlsx')
     print("Data successfully added to spreadsheet")
@@ -33,8 +33,20 @@ if type == "1":
     print("would you like to add this data to a spreadsheet?")
     answer = input("Please enter yes or no: ") # User input to choose between yes or no
     if answer == "yes":
-        insert_new_data(kilometresDriven,litresUsed)
+        insert_new_data(kilometresDriven, litresUsed)
 elif type == "2": #summary data 
     book = load_workbook('testDocument.xlsx') # Load workbook
     sheet = book.active # Load active sheet
+    dict = {}
+    for row in sheet.iter_rows():
+        if row[0].value not in dict.keys():
+            temp = {str(row[0].value): [row[1].value, row[2].value]}
+            dict.update(temp)
+        else:
+            dict[row[0].value].append(row[1].value)
+            dict[row[0].value].append(row[2].value)
+    for key, value in dict.items():
+        print(key, value)
+        
+    
     
