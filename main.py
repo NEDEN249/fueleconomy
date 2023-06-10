236b035654215a6dd1d7f1691accf64a2e945b92
nathan eden <nathaneden483@yahoo.com.au>
2023-06-10 21:35:45 +0800

    rounding fixed, now displays graph

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import xlwt
 import openpyxl
 from openpyxl import Workbook, load_workbook
 from datetime import date
+import matplotlib.pyplot as plt
 
 today = date.today()
 today1 = today.strftime("%d/%m/%Y")
@@ -19,6 +20,9 @@ def insert_new_data(d,f):
     book.save('testDocument.xlsx')
     print("Data successfully added to spreadsheet")
 
+def printFuelEconomy(fuelEconomy):
+    print("\nYour fuel economy is {:0.2f} litres per 100kms\n".format(fuelEconomy)) # Print fuel economy")
+
 print("Welcome to the Fuel Economy Calculator") # Welcome message
 print("=======================================")
 print("what would you like to do?")
@@ -29,16 +33,19 @@ if type == "1":
     kilometresDriven = input("Please enter the number of kilometres driven: ") # User input for kilometres driven
     litresUsed = input("Please enter the number of litres used: ") # User input for litres used
     fuelEconomy = calculate_fuel_economy(float(kilometresDriven), float(litresUsed)) # Calling the function to calculate fuel economy
-    print("Your fuel economy is: " + str(fuelEconomy) + " litres per 100km") # Print fuel economy
+    printFuelEconomy(fuelEconomy)
     print("would you like to add this data to a spreadsheet?")
     answer = input("Please enter yes or no: ") # User input to choose between yes or no
     if answer == "yes":
         insert_new_data(kilometresDriven, litresUsed)
+    #elif answer == "no": -> ask if want to see summary data 
 elif type == "2": #summary data 
     book = load_workbook('testDocument.xlsx') # Load workbook
     sheet = book.active # Load active sheet
     dict = {}
     for row in sheet.iter_rows():
+        if row[0].value == "Date":
+            continue # Skip
         if row[0].value not in dict.keys():
             temp = {str(row[0].value): [row[1].value, row[2].value]}
             dict.update(temp)
@@ -48,6 +55,16 @@ elif type == "2": #summary data
             dict[row[0].value].append(row[2].value)
     for key, value in dict.items():
         print(key, value)
-        
+    averages = []
+    for key, value in dict.items():
+        print(key)
+        if key == "Date":
+            continue # Skip the first row
+        print(value[0])
+        print(value[1])
+        averages.append(calculate_fuel_economy(int(value[0]), int(value[1])))
+    fig,ax = plt.subplots()
+    ax.bar(dict.keys(), averages)
+    plt.show()
     
     
