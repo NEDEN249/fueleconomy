506fb965d44f722c6a33ea359d929067e44dd3e6
nathan eden <nathaneden483@yahoo.com.au>
2023-06-13 16:10:05 +0800

    can input date (instead of default to current)

--- functions.py
+++ functions.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 
 #Current Date in DD/MM/YYYY format
 today = date.today()
-today1 = today.strftime("%d/%m/%Y")
+dateToday = today.strftime("%d/%m/%Y")
+#today2 = "14/06/2023"
 
 def calculate_fuel_economy(kilometresDriven, litresUsed): # Function to calculate fuel economy by litres per 100km
     fuelEconomy =  (litresUsed / kilometresDriven) * 100
     return fuelEconomy
 
-def insert_new_data(distance, fuel): #Function to insert new data into spreadsheet
+def insert_new_data(date, distance, fuel, price): #Function to insert new data into spreadsheet
     try: # Try to open spreadsheet
         book = load_workbook('fuelEconomyData.xlsx') # Load workbook
         sheet = book.active # Load active sheet
@@ -22,10 +23,14 @@ def insert_new_data(distance, fuel): #Function to insert new data into spreadshe
         worksheet.write("A1", 'Date')# Add column headers
         worksheet.write("B1", 'Distance')
         worksheet.write("C1", 'Fuel')
+        worksheet.write("D1", 'Price')
         workbook.close() # Close workbook
         book = load_workbook('fuelEconomyData.xlsx')
         sheet = book.active
-    new_data = (today1, int(distance), int(fuel)) # Insert the new data into the spreadsheet
+    if date == "today":
+        new_data = (dateToday, float(distance), float(fuel), float(price))
+    else:
+        new_data = (date, float(distance), float(fuel), float(price)) # Insert the new data into the spreadsheet
     sheet.append(new_data)
     book.save('fuelEconomyData.xlsx') # Save changes
     print("Data successfully added to spreadsheet")
@@ -36,12 +41,18 @@ def printFuelEconomy(fuelEconomy): # Print fuel economy to 2 decimal places
 def returnFuelEconomy():
     kilometresDriven = input("Please enter the number of kilometres driven: ") # User input for kilometres driven
     litresUsed = input("Please enter the number of litres used: ") # User input for litres used
+    price = input("Please enter the price of fuel: ") # User input for price of fuel
+    dateChoice = input("do you want to use todays date? (yes or no): ") # User input to choose between yes or no
+    if dateChoice == "no":
+        finalDate = input("please enter the date in DD/MM/YYYY format: ")
+    else:
+        finalDate = "today"
     fuelEconomy = calculate_fuel_economy(float(kilometresDriven), float(litresUsed)) # Calling the function to calculate fuel economy
     printFuelEconomy(fuelEconomy)
     print("would you like to add this data to a spreadsheet?")
     answer = input("Please enter yes or no: ") # User input to choose between yes or no
     if answer == "yes":
-        insert_new_data(kilometresDriven, litresUsed)
+        insert_new_data(finalDate, kilometresDriven, litresUsed, price)
     #elif answer == "no": -> ask if want to see summary data 
     
 def generateSummaryData():
